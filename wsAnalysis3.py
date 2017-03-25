459fa7722a9f9da5d38447985100ee785eb2d8fd
Ben Mastay <Ben@Bens-MacBook-Pro-2.local>
2017-03-25 13:38:17 -0400

    Cleaned up code a bit

--- wsAnalysis3.py
+++ wsAnalysis3.py
@@ -22,45 +22,7 @@ def CalcNorm(df, label, dimensions):
     v = df.loc[:,[label+d for d in dimensions]]
     return np.linalg.norm(v, axis=1)
 
-def myPlot(df, title):
-    plt.figure(); 
-    plt.subplot(311); 
-    df['speed_error'].plot(); plt.ylabel('Speed Error'); plt.xlabel('Timestamp') 
-    plt.title(title)
-    plt.subplot(312); 
-    df['delta.time'].plot(); plt.ylabel('Time Delta'); plt.xlabel('Timestamp') 
-    plt.subplot(313); 
-    df['delta.time'].plot.kde(); plt.xlabel('Time Delta')
-
-def myScatterPlot(df, title):
-    plt.figure(); 
-    plt.subplot(311); 
-    df['speed_error'].plot(style='o'); plt.ylabel('Speed Error'); plt.xlabel('Timestamp')
-    plt.title(title)
-    plt.subplot(312); 
-    df['delta.time'].plot(style='o'); plt.ylabel('Time Delta'); plt.xlabel('Timestamp')
-    plt.subplot(313); 
-    df['delta.time'].plot.kde(); plt.xlabel('Time Delta')
-
-def myDualScatterPlot(df, title):
-    fig, ax = plt.subplots()
-    ax1 = ax
-    ax1.set_xlabel('Timestamp')
-    ax1_b = ax1.twinx()
-    ax1.set_ylabel('Speed Error', color='b')
-    ax1.tick_params('y', colors='b')
-    ax1_b.set_ylabel('Time Delta', color='r')
-    ax1_b.tick_params('y', colors='r')
-    df['speed_error'].plot(ax=ax1, style='bv', alpha=0.5)
-    df['delta.time'].plot(ax=ax1_b, style='r^', alpha=0.5)
-    plt.title(title)
-    plt.tight_layout()
-    
-
-
-if __name__ == "__main__":
-    df = DataframeFromFile(sys.argv[1])
-
+def ProcessData(df):
     df['delta.time'] = GetOneIndexedDiff(df, 'dis.timestamp')
 
     dims = ['x','y','z']
@@ -75,22 +37,54 @@ if __name__ == "__main__":
     df['error_magnitude'] = df['speed_error'].map(np.fabs)
 
     df = df.set_index('dis.timestamp')
+    return df
 
-    myPlot(df, 'All Data')
+def SummaryPlot(df, title):
+    plt.figure(); 
+    plt.subplot(311); 
+    df['speed_error'].plot(); plt.ylabel('Speed Error'); plt.xlabel('Timestamp') 
+    plt.title(title)
+    plt.subplot(312); 
+    df['delta.time'].plot(); plt.ylabel('Time Delta'); plt.xlabel('Timestamp') 
+    plt.subplot(313); 
+    df['delta.time'].plot.kde(); plt.xlabel('Time Delta')
+
+def DualScatterPlot(df, title):
+    fig, ax1 = plt.subplots()
+    ax1.set_xlabel('Timestamp')
+    ax2 = ax1.twinx()
+    ax1.set_ylabel('Speed Error', color='b')
+    ax1.tick_params('y', colors='b')
+    ax2.set_ylabel('Time Delta', color='r')
+    ax2.tick_params('y', colors='r')
+    df['speed_error'].plot(ax=ax1, style='bv', alpha=0.5)
+    df['delta.time'].plot(ax=ax2, style='r^', alpha=0.5)
+    plt.title(title)
+    plt.tight_layout()
+    
+def PlotData(df):
+    SummaryPlot(df, 'All Data')
 
     smallDT = df[df['delta.time'] < 0.2]
-    myPlot(smallDT, 'Time Deltas < 0.2')
+    SummaryPlot(smallDT, 'Time Deltas < 0.2')
 
     verySmallDT = df[df['delta.time'] < 0.05]
-    myDualScatterPlot(verySmallDT, 'Time Deltas < 0.05')
+    DualScatterPlot(verySmallDT, 'Time Deltas < 0.05')
 
     unacceptableError = df[df['error_magnitude'] > 0.2]
-    myDualScatterPlot(unacceptableError, 'Error Magnitude > 0.2')
-
+    DualScatterPlot(unacceptableError, 'Error Magnitude > 0.2')
 
     highError = df[df['error_magnitude'] > 0.5]
-    myDualScatterPlot(highError, 'Error Magnitude > 0.5')
-
+    DualScatterPlot(highError, 'Error Magnitude > 0.5')
+    
     plt.show()
+
+if __name__ == "__main__":
+    df = DataframeFromFile(sys.argv[1])
+
+    df = ProcessData(df)
+
+    PlotData(df)
+
 
     
